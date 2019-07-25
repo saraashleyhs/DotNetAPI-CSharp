640fb337ffa5185b3498b8d1d5124bb61864d8ca
saraashleyhs <sara.ashleyhs@gmail.com>
2019-07-24 20:49:35 -0500

    MY first API

---
+++ DotNetAPI-Projects/MyFirstAPI/Controllers/PeopleController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using MyFirstAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private static readonly List<Person> _people = new List<Person>
+        {
+            new Person
+            {
+                Id = 1,
+                Name = "Luke Skywalker",
+                HairColor = "blond"
+            },
+            new Person
+            {
+                Id = 5,
+                Name = "Leia Organa",
+                HairColor = "brown"
+            }
+        };
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_people);
+        }
+
+        // GET api/values/5
+        // GET api/people/5
+        [HttpGet("{id}")]
+        [Produces("application/json")]
+        public IActionResult Get(int id)
+        {
+            var person = _people.FirstOrDefault(p => p.Id == id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // POST api/values
+        // POST api/people
+        [HttpPost]
+        public IActionResult Post([FromBody] Person newPerson)
+        {
+            _people.Add(newPerson);
+            return CreatedAtAction("Get", newPerson, new { id = new Random().Next() });
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var person = _people.FirstOrDefault(p => p.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            _people.Remove(person);
+            return Ok(_people);  //if you pass person through there it will show you what was deleted.
+        }
+    }
+}
---
+++ DotNetAPI-Projects/MyFirstAPI/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyFirstAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+
+        //public Person(int id, string name, string haircolor)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    HairColor = haircolor;
+        //}
+    }
+
+}
